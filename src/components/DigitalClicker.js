6bd2226c47d3d3e8774aabaa36c2491ddbec3e59
Asilver-jpg <asilver7493@gmail.com>
2020-05-12 20:03:20 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,18 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+export default class DigitalClicker extends React.Component{
+
+    state= {
+        timesClicked :0
+    }
+render(){
+    return(<button onClick= {this.incrementCounter} label= {this.state.timesClicked}>{this.state.timesClicked}</button>
+    )
+}
+
+incrementCounter= ()=>{
+    this.setState(previousState=> {return{timesClicked: previousState.timesClicked +1
+    }
+})
+}
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,29 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+ export default class YouTubeDebugger extends React.Component{
+    state= {
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+    }
+    render(){
+        return(<div><button onClick={this.setBitrate} className="bitrate">Set Bitrate</button>
+        <button onClick= {this.setResolution} className="resolution">Set Resolution</button></div>)
+    }
+    setResolution= () => {
+        this.setState({settings:{ video: {resolution : '720p'}}})
+        console.log(this.state.settings.video.resolution)
+    }
+
+    setBitrate=()=>{
+        this.setState({settings: {...this.state.settings, bitrate: 12}})
+        console.log(this.state.settings.bitrate)
+
+    }
+}
